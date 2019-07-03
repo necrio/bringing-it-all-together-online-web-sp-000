707bdea674d0da180fcc7820ec030e297b819cf5
Keith Isham <kisham8899@gmail.com>
2019-07-03 18:21:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,4 @@ class Dog
   
   def initialize(attributes)
     #id: nill, :name, :breed
-    attributes.each do {|k, v| self.send(("#{key}"), value)
+    attributes.each do {|k, v| self.send(("#{key}"), value)}
